943b416552e9d7e3a341c1700823f5d2ba280c68
adam <adam.bukovsek@fwamstetten.ac.at>
2025-05-26 16:02:52 +0200

    Created the Login Page Prototype

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from "../components/Login.tsx";
+
+const LoginPage: React.FC = () => {
+  return (
+    <>
+      <div className="App">
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
