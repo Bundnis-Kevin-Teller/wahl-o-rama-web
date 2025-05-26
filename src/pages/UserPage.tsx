245fc3dab9086252c95d26b833dca21400370a37
adam <adam.bukovsek@fwamstetten.ac.at>
2025-05-26 16:03:00 +0200

    Created the User Page Prototype

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserPage: React.FC = () => {
+    return (
+        <>
+            <div className="App">
+
+            </div>
+        </>
+    );
+}
+
+export default UserPage;
