1b820b88ddb85aaa266919e3eed575b4c94d986c
adam <adam.bukovsek@fwamstetten.ac.at>
2025-05-26 18:01:06 +0200

    Created user data file

---
+++ src/data/users.ts
@@ -0,0 +1,12 @@
+import { User } from "../types"
+
+export const Users: User[] = [
+    {
+        id: 1,
+        name: "Martina Weber",
+        email: "martina@gmail.com",
+        role: "Kanzlerkandidatin",
+        avatar: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=150",
+        passwordHash: "",
+    }
+]
